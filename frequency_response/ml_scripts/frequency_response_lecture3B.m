883bf462b1271fbce3d5b2083faefe6861eaca10
thill <thill@tntech.edu>
2020-04-25 19:05:31 -0500

    getting close

---
+++ frequency_response/ml_scripts/frequency_response_lecture3B.m
@@ -0,0 +1,23 @@
+% ME3050 - Spring 2020 Tennessee Technological University
+% Tristan Hill - 04/19/2020
+clear variables;clc;close all
+
+% define the system parameters
+% this is an underdamped system
+
+m=2;c=1;k=20;
+
+c^2-4*m*k
+
+zeta=c/(2*sqrt(m*k));
+
+% define the amplitude 
+A=1;
+
+
+% use built-in MATLAB Bode Plot
+figure(1)
+sys=tf(1/k,[(m/k) (c/k) 1]);
+bode(sys);grid on
+str=sprintf('Bode Diagram, zeta=%.2f',zeta);
+title(str)
