825df9adefdc1fce8f065b2cba5688cb053695c0
thill <thill@tntech.edu>
2023-03-23 16:25:27 -0500

    added dc_motor_state_space.m

---
+++ electrical_systems/electical_systems_topic4/dc_motor_state_space.m
@@ -0,0 +1,36 @@
+% ME3050 - Dynamics Modeling and Controls
+% Tristan Hill - Spring 2023
+% DC Motor Example (System Dynamics 4th ed., Palm, Pg 376-378)  
+clear; close all; clc
+
+% define system parameters
+KT=0.05;  % (N*m/A)
+Kb=KT;    % (N*m/A)
+c=10e-4;  % (N*m*s/rad)   
+Ra=0.5;   % (Ohm)
+La=2e-3;  % (H)
+I=9e-5;   % (kg*m^2)
+
+
+
+% define components of the state equation
+A=[-Ra/La -Kb/La
+   KT/I -c/I];
+% B matrix is 2x2 because u vector is 2x1
+B=[1/La 0 
+   0 -1/I];
+
+% use first two states as outputs
+C=[1 0
+   0 1];
+% the D matrix shape of B matrix 
+D=[0 0
+   0 0];
+
+% create a state space model object
+sys1=ss(A,B,C,D);
+
+% simulate a step response 
+figure(1)
+step(sys1)
+grid on; xlabel('time(s)')
